193d5645a1e7add10e31a113aa4e6d48b56f6f6e
MariaSliufko <89866298+MariaSliufko@users.noreply.github.com>
2022-05-26 12:16:29 +0200

    Create CreateUserHighscore.sql (#2)
    Added database table Users and HighScore.

---
+++ SQLQueries/CreateUserHighscore.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE cityQuizDB;
+
+CREATE TABLE Users (
+    id int IDENTITY,
+    userName varchar(255) NOT NULL,
+    password varchar(20),
+    timetaken int,
+    score int,
+    PRIMARY KEY (id)
+);
+GO
+
+CREATE TABLE HighScore (
+    id int IDENTITY,
+    userid  int,
+    score int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userid) REFERENCES Users(id)
+);
+GO
