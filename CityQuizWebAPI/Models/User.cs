601314213971cc21ebf180d0fba4988d1d3d928e
ida <idasusannajohansson@gmail.com>
2022-05-26 15:45:45 +0200

    Scaffolded DB

---
+++ CityQuizWebAPI/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CityQuizWebAPI.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            HighScores = new HashSet<HighScore>();
+        }
+
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int? Timetaken { get; set; }
+        public int? Score { get; set; }
+
+        public virtual ICollection<HighScore> HighScores { get; set; }
+    }
+}
