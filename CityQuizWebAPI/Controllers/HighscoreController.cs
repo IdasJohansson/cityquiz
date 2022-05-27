b84d818137d366086b5d50ea9e1b7756e1ab9d4c
MariaSliufko <89866298+MariaSliufko@users.noreply.github.com>
2022-05-27 10:04:34 +0200

    Added controllers (#6)
    Added controllers

---
+++ CityQuizWebAPI/Controllers/HighscoreController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityQuizWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HighscoreController : ControllerBase
+    {
+        // GET: api/Highscore
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Highscore/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Highscore
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Highscore/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/Highscore/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
