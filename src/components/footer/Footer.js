5e449fdf412169c3da1f0b1d62980edf07d49a1e
MariaSliufko <89866298+MariaSliufko@users.noreply.github.com>
2022-05-31 09:27:32 +0200

    minorChangesFooter (#21)

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Row, Container, Col } from 'reactstrap';
 import "./Footer.css";
 
 export const Footer = () => {
@@ -18,23 +17,23 @@ export const Footer = () => {
                
                     <h3 className='headlines'>Contact Us</h3>
                     <ul className="social-list">
-                    <li class="social-list__item"><a class="social-list__link"
-                    href="https://www.linkedin.com/in/wilma-hammarshall/" target="_blank">Wilmas LinkedIn</a></li>
-                    <li class="social-list__item"><a class="social-list__link"
-                    href="https://www.linkedin.com/in/ida-s-johansson/" target="_blank">Idas LinkedIn</a></li>
-                   <li class="social-list__item"><a class="social-list__link"
-                   href="https://www.linkedin.com/in/maria-sliufko-b8773749/" target="_blank">Marias LinkedIn</a></li>
+                    <li className="social-list__item"><a className="social-list__link"
+                    href="https://www.linkedin.com/in/wilma-hammarshall/" target="_blank">Wilma</a></li>
+                    <li className="social-list__item"><a className="social-list__link"
+                    href="https://www.linkedin.com/in/ida-s-johansson/" target="_blank">Ida</a></li>
+                   <li className="social-list__item"><a className="social-list__link"
+                   href="https://www.linkedin.com/in/maria-sliufko-b8773749/" target="_blank">Maria</a></li>
                    </ul>
                
                 
                     <h3 className='headlines'>View our projects</h3>
                     <ul className="social-list">
-                    <li class="social-list__item"><a class="social-list__link"
-                    href="https://github.com/Hammarshall" target="_blank">Wilmas GitHub</a></li>
-                    <li class="social-list__item"><a class="social-list__link"
-                    href="https://github.com/IdasJohansson" target="_blank">Idas GitHub</a></li>
-                   <li class="social-list__item"><a class="social-list__link"
-                   href="https://github.com/MariaSliufko" target="_blank">Marias GitHub</a></li>
+                    <li className="social-list__item"><a className="social-list__link"
+                    href="https://github.com/Hammarshall" target="_blank">Wilma</a></li>
+                    <li className="social-list__item"><a className="social-list__link"
+                    href="https://github.com/IdasJohansson" target="_blank">Ida</a></li>
+                   <li className="social-list__item"><a className="social-list__link"
+                   href="https://github.com/MariaSliufko" target="_blank">Maria</a></li>
                    </ul>
                
             
