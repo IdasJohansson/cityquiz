17f0a656d9a7305146db4e0074a2120500f8083b
ida <idasusannajohansson@gmail.com>
2022-06-01 09:06:36 +0200

    Added questionmarklogo in navbar
    Obs! Placed the logo inside img-folder.

--- src/components/navigation/navigationdesktop/NavigationDesktop.js
+++ src/components/navigation/navigationdesktop/NavigationDesktop.js
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 import RoutingPath from "../../../routes/RoutingPath";
 import { UserContext } from "../../../shared/provider/UserProvider";
 import { useContext } from "react";
+import logo512 from "../../img/logo512.png"
 
 export const NavigationDesktop = () => {
     const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext);
     
     return (
         <nav className="nav-desktop">
-            <span className="nav-desktop-logo"> Insert logo here </span>
+            <span className="nav-desktop-logo"> <img className='icon' src ={logo512} alt = "Questionmarklogo"/> </span>
             <span> Logged in user: {authenticatedUser}</span>
             <ul className="nav-desktop-list">
                 <li className="nav-desktop-link">
