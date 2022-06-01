b76ed3f8652d927a12511d24ba1e16768e44303e
ida <idasusannajohansson@gmail.com>
2022-06-01 12:51:24 +0200

    Added questionmark logo to be visual before map will show in quizview

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,7 +1,13 @@
 import "./map.css"
+import logo512 from "../img/logo512.png"
 
 export const Map = () => {
 
     return (
-    <div className="googleMap"> </div>); 
+    <div className="googleMap">
+
+         <img className='icon-questionmark' src ={logo512} alt = "Questionmarklogo"/> 
+
+         
+    </div>); 
     }; 
--- src/view/QuizView/QuizView.js
+++ src/view/QuizView/QuizView.js
@@ -10,11 +10,11 @@ export const QuizView = () => {
     return (
         <>  
         {/*Lägg in villkor att quiz-container visar 10 frågor, men sen skickas man till highscoreview */ }
-        <div className="container">
+        <div className="quiz-container">
         <div>
             <h1> LET'S QUIZ</h1>
         </div>
-            <div> Insert googlemaps picture here (Som visar vart på kartan det rätta svaret ligger, den blåa rutan symboliserar Detta)</div>
+            <div> Insert googlemaps picture here (Som visar vart på kartan det rätta svaret ligger, rutan symboliserar vart kartan kommer att ligga)</div>
             <Map></Map>
             <Quiz></Quiz>
         </div>
