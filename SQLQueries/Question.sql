679f08b25be196452c7923ac67e059b864feec01
Wilma Hammarshall <wilmahammarshall@gmail.com>
2022-06-07 12:22:50 +0200

    fixed coordinates

--- SQLQueries/Question.sql
+++ SQLQueries/Question.sql
@@ -15,64 +15,64 @@ GO
 SET IDENTITY_INSERT Question ON
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (1, 'Vojkovice', 'Czech Republic', 18.468024, 49.6823404);
+INSERT INTO Question (id, city, country, longitude, latitude) values (1, 'Vojkovice', 'Czech Republic', 18.47311619704032, 49.68561642049438); /*49.68561642049438, 18.47311619704032*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (2, 'Karangarjo', 'Indonesia', 111.6198661, -6.7050629);
+INSERT INTO Question (id, city, country, longitude, latitude) values (2, 'Karangarjo', 'Indonesia', 114.02135779712958, -8.36834944206266); /*-8.36834944206266, 114.02135779712958*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (3, 'Urbiztondo', 'Philippines', 120.349903, 15.816699);
+INSERT INTO Question (id, city, country, longitude, latitude) values (3, 'Urbiztondo', 'Philippines', 120.32269381737817, 15.841668721416605); /*15.841668721416605, 120.32269381737817*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (4, 'Somorpenang', 'Indonesia', 114.0612, -6.9949);
+INSERT INTO Question (id, city, country, longitude, latitude) values (4, 'Espinosa', 'Brazil', -42.811489796176645, -14.919944430300674); /*-14.919944430300674, -42.811489796176645*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (5, 'Neuville', 'Canada', -71.58275, 46.69823);
+INSERT INTO Question (id, city, country, longitude, latitude) values (5, 'Neuville', 'Canada', -71.63254526440504, 46.719514220955325); /*46.719514220955325, -71.63254526440504*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (6, 'Namyangju', 'South Korea', 127.2165279, 37.6360028);
+INSERT INTO Question (id, city, country, longitude, latitude) values (6, 'Namyangju', 'South Korea', 127.24076501375265, 37.65886927830609); /*37.65886927830609, 127.24076501375265*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (7, 'Hongling', 'China', 114.103143, 22.54921);
+INSERT INTO Question (id, city, country, longitude, latitude) values (7, 'Onguday', 'Russia', 86.13680902611095, 50.74974725781954); /*50.74974725781954, 86.13680902611095*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (8, 'Jinqu', 'China', 107.788999, 34.208655);
+INSERT INTO Question (id, city, country, longitude, latitude) values (8, 'Carania', 'Peru', -75.87081219479252, -12.344553147646701); /*-12.344553147646701, -75.87081219479252*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (9, 'Fonte Boa', 'Brazil', -66.1415263, -2.4681009);
+INSERT INTO Question (id, city, country, longitude, latitude) values (9, 'Fort Dauphin', 'Madagascar', 46.9755107250183, -25.02507560372845); /*-25.02507560372845, 46.9755107250183*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (10, 'Picoto', 'Portugal', -8.6358837, 41.0734417);
+INSERT INTO Question (id, city, country, longitude, latitude) values (10, 'Picoto', 'Portugal', -8.364497946272905, 41.55956450225868); /*41.55956450225868, -8.364497946272905*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (11, 'Kabir', 'Indonesia', 124.2227061, -8.2481387);
+INSERT INTO Question (id, city, country, longitude, latitude) values (11, 'Kabir', 'Netherlands', 6.6486394301051055, 52.37017726815807); /*52.37017726815807, 6.6486394301051055*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (12, 'Houping', 'China', 118.481377, 26.608357);
+INSERT INTO Question (id, city, country, longitude, latitude) values (12, 'Cuyotenango', 'Guatemala', -97.57522091491676, 14.541682898076518); /*14.541682898076518, -91.57522091491676*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (13, 'Longtan', 'China', 117.017536, 25.075123);
+INSERT INTO Question (id, city, country, longitude, latitude) values (13, 'Sheshory', 'Ukraine', 24.945422915638026, 48.32326201171657); /*48.32326201171657, 24.945422915638026*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (14, 'Nancang', 'China', 115.858197, 28.682892);
+INSERT INTO Question (id, city, country, longitude, latitude) values (14, 'Mongar', 'Bhutan', 91.23690610843383, 27.277822806500804); /*27.277822806500804, 91.23690610843383*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (15, 'Lameira', 'Portugal', -8.6240851, 41.7811503);
+INSERT INTO Question (id, city, country, longitude, latitude) values (15, 'Neiafu', 'Tonga', -173.98236753915424, -18.646143032830246); /*-18.646143032830246, -173.98236753915424*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (16, 'Muang Xay', 'Laos', 101.9861694, 20.690696);
+INSERT INTO Question (id, city, country, longitude, latitude) values (16, 'Muang Xay', 'Laos', 101.99642632090301, 20.694700143243423); /*20.694700143243423, 101.99642632090301*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (17, 'Nong Chik', 'Thailand', 101.1843887, 6.8117856);
+INSERT INTO Question (id, city, country, longitude, latitude) values (17, 'Nong Chik', 'Thailand', 101.15900799587449, 6.82616402294431); /*6.82616402294431, 101.15900799587449*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (18, 'Murmashi', 'Russia', 32.7986688, 68.8128001);
+INSERT INTO Question (id, city, country, longitude, latitude) values (18, 'Kato Nevrokopi', 'Greece', 23.869634162923095, 41.342453957101014); /*41.342453957101014, 23.869634162923095*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (19, 'Paris 19', 'France', 5.8978018, 43.4945737);
+INSERT INTO Question (id, city, country, longitude, latitude) values (19, 'Remiremont', 'France', 6.580640356414576, 48.00144001709567); /*48.00144001709567, 6.580640356414576*/
 GO
 
-INSERT INTO Question (id, city, country, longitude, latitude) values (20, 'Igurusi', 'Tanzania', 33.8425243, -8.8221596);
+INSERT INTO Question (id, city, country, longitude, latitude) values (20, 'Igurusi', 'Tanzania', 33.84314774377946, -8.821932039993673); /*-8.821932039993673, 33.84314774377946*/
 GO
 
 
--- src/shared/api/service/LocalhostAPIService.js
+++ src/shared/api/service/LocalhostAPIService.js
@@ -1,14 +1,17 @@
 import http from "../LocalhostAPI";
 
-// Hämtar ett userobject från databasen, jämför med inputfälten i login. Svarar med true om det finns en användare med samma användarnamn och lösen. 
+// Hämtar ett userobject från databasen, jämför med inputfälten i login. Svarar med true om det finns en användare med samma användarnamn och lösen.
 export const getLoginCheck = async (user) => {
-    const response = await fetch("https://localhost:5001/api/User/CheckLogin",{
-    method:"POST",    
-    headers:{"content-type":"application/json"}, body:JSON.stringify({UserName:user.inputusername,Password:user.inputpassword})
-    })
-    return response.json()
-}; 
-
+  const response = await fetch("https://localhost:5001/api/User/CheckLogin", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      UserName: user.inputusername,
+      Password: user.inputpassword,
+    }),
+  });
+  return response.json();
+};
 
 /*
 // Hämtar ett questionobject
@@ -21,23 +24,19 @@ export const getQuestion = async (question) => {
 }; 
 */
 
-
 const getQuestion = async () => {
-    return await http.get("Question/1")
-}
-export {getQuestion}
+  return await http.get("Question/1");
+};
+export { getQuestion };
 
 // Hämtar ett random Question object
 const getRandomQuestion = async () => {
-    return await http.get("Question/Random")
-}
-export {getRandomQuestion}
+  return await http.get("Question/Random");
+};
+export { getRandomQuestion };
 
-
-// Ska kolla om man har svarat rätt i db, returnerar true eller false. 
+// Ska kolla om man har svarat rätt i db, returnerar true eller false.
 const getAnswerCheck = async () => {
-    return await http.get("Question/CheckAnswer")
-}; 
-export {getAnswerCheck}; 
-
-
+  return await http.get("Question/CheckAnswer");
+};
+export { getAnswerCheck };
