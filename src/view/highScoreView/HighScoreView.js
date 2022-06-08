ea464f172acf86a2a7a2a3964e3ef33f2fa6987a
ida <idasusannajohansson@gmail.com>
2022-06-08 12:09:11 +0200

    Countprovider is working

--- src/view/highScoreView/HighScoreView.js
+++ src/view/highScoreView/HighScoreView.js
@@ -1,27 +1,20 @@
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-//osäker på hur importen ska ligga här
 import "./highScoreView.css";
 import LocalStorage from "../../shared/storage/LocalStorage";
 import "../../index.css";
 import RoutingPath from "../../routes/RoutingPath";
 import { UserContext} from "../../shared/provider/UserProvider";
+import { CountContext } from "../../shared/provider/CountProvider";
 
 export const HighScoreView = () => {
   const navigate = useNavigate(); // Ok
   const [AuthenticatedUser, setAuthenticatedUser] = useContext(UserContext); // Ok. 
-  const [count, setCount] = useState(0); // Behöver bli global så att man kommer åt i highscore
-  const [questionNumber, setQuestionNumber] = useState(1); // Behlöver bli global så att man kommer åt i Highscore
+  const [count, setCount] = useContext(CountContext); // Behöver bli global så att man kommer åt i highscore
 
-  // Count Ska bli en global variabel.
-  // QuestionNumber Ska bli en global variabel.
-  
   const restartGame = () => {
-    
     //om man vill köra igen så ska count nollställas
-    //setCount(0);nollställer count, ska bli global
-    //setQuestionNumber(0);nollställer antal frågor 
-  
+    //setCount(0);nollställer count
     navigate(RoutingPath.quizView); // Skickar vidare till Quiz, ok. 
   };
 
@@ -43,21 +36,17 @@ export const HighScoreView = () => {
           <h1>HIGHSCORE</h1>
         </div>
         <div className="final-results">
-          {" "}
-          {/*namnet på classen där alla resultat visas*/}
-          <h2>Final Results</h2>
-          <h3>
 
-            {/* Här vill vi visa antal poäng (count) utav antal frågor (10) */ }
-            {/*
-            {count} out of {questions.length} correct - (
-            {(count / questions.length) * 100}%){" "}
-            */}
+ 
+          <h2>Final Results</h2>
+
+          <h3>
+            You scored {count} out of 10 questions. 
           </h3>
-          {/*skriver ut hur många rätt svar som har gjorts av totalt antal frågor*/}
+
           <div className="top3">
             <h3>
-              1. {/* Här vill vi skriva ut det bästa resultatet, poäng och användarnamn? */}
+              1. {/* Här vill vi skriva ut det bästa resultatet, poäng och användarnamn? dett ska lagras i databasen */}
               <br />
               2.
               <br />
@@ -79,10 +68,3 @@ export const HighScoreView = () => {
 };
 
 
-/*
-          /*startar om spelet*/
-          /*LÄGG TILL ATT VISA TOP 3*/
-          /*LÄGG TILL logga ut / avsluta spel knapp*/
-          /*ngn funktion för att få in tätt värde på rätt pats top 3
-
-*/
